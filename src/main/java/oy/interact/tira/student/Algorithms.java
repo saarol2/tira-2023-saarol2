77cc1231344e32d653a49875e0bf25d52b68f035
Oliver Saari <oliver.saari@student.oulu.fi>
2023-10-23 21:41:47 +0300

    task6 (OutOfMemoryError)

--- src/main/java/oy/interact/tira/student/Algorithms.java
+++ src/main/java/oy/interact/tira/student/Algorithms.java
@@ -143,6 +143,7 @@ public class Algorithms {
    }
 
    public static <E> void fastSort(E [] array, int fromIndex, int toIndex, Comparator<E> comparator) {
+      //Toteutettu Hoaren quicksort algoritmillä
       if (fromIndex < toIndex){
          int partitionIndex = partition(array, fromIndex, toIndex, comparator);
          fastSort(array, fromIndex, partitionIndex, comparator);
@@ -170,26 +171,6 @@ public class Algorithms {
       }
    }
 
-   private static <E extends Comparable<E>> int partition(E[] array, int fromIndex, int toIndex){
-      E pivot = array[(toIndex - fromIndex) / 2 + fromIndex];
-      int low = fromIndex - 1;
-      int high = toIndex + 1;
-      while (true){
-         do{
-            low++;
-         }
-         while (array[low].compareTo(pivot) < 0);
-         do{
-            high--;
-         }
-         while (array[high].compareTo(pivot) > 0);
-         if (low >= high){
-            return high;
-         }
-         swap(array, low, high);
-      }
-   }
-   
    private static <E> void swap(E[] array, int i, int j){
       E temp = array[i];
       array[i] = array[j];
