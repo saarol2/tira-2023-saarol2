f5d7c2ac6df0cfb646e2452f3625011971159d6c
Oliver Saari <oliver.saari@student.oulu.fi>
2024-01-07 14:25:43 +0200

    final fix

--- src/main/java/oy/interact/tira/student/CodeWordsCounter.java
+++ src/main/java/oy/interact/tira/student/CodeWordsCounter.java
@@ -8,7 +8,6 @@ import java.nio.file.PathMatcher;
 import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.Arrays;
 import java.util.Comparator;
 
 import oy.interact.tira.factories.HashTableFactory;
